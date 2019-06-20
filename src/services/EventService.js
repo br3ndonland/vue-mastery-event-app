c8faae509e357670c7ac699f277cb88717363c3b
Brendon Smith <bsmith@invicro.com>
2019-06-20 16:40:03 -0400

    Update API port in EventService.js
    Using port 8081 for the json-server so it doesn't conflict with other
    applications running on localhost

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://localhost:8081`,
   withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
