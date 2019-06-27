32fe3bd66a9b01c8e16ad3cbfafc2dd0f4b451ae
Brendon Smith <bsmith@invicro.com>
2019-06-27 16:54:55 -0400

    Refactor store.js actions with async/await

--- src/store.js
+++ src/store.js
@@ -35,37 +35,30 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    createEvent({ commit }, event) {
-      return EventService.postEvent(event).then(() => {
-        commit('ADD_EVENT', event)
-      })
+    async createEvent({ commit }, event) {
+      await EventService.postEvent(event)
+      commit('ADD_EVENT', event)
     },
-    fetchEvents({ commit }, { perPage, page }) {
-      EventService.getEvents(perPage, page)
-        .then(response => {
-          commit(
-            'SET_EVENTS_TOTAL',
-            parseInt(response.headers['x-total-count'])
-          )
-          commit('SET_EVENTS', response.data)
-        })
-        .catch(error => {
-          console.log('There was an error:', error.response)
-        })
-    },
-    fetchEvent({ commit, getters }, id) {
-      var event = getters.getEventById(id)
-
+    async fetchEvent({ commit, getters }, id) {
+      const event = getters.getEventById(id)
       if (event) {
         commit('SET_EVENT', event)
       } else {
-        EventService.getEvent(id)
-          .then(response => {
-            commit('SET_EVENT', response.data)
-          })
-          .catch(error => {
-            console.log('There was an error:', error.response)
-          })
+        try {
+          let response = await EventService.getEvent(id)
+          commit('SET_EVENT', response.data)
+        } catch (e) {
+          throw Error(e)
+        }
+      }
+    },
+    async fetchEvents({ commit }, { perPage, page }) {
+      try {
+        let response = await EventService.getEvents(perPage, page)
+        commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']))
+        commit('SET_EVENTS', response.data)
+      } catch (e) {
+        throw Error(e)
       }
     }
   },
